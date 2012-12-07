a006a75b63c6d12a80e157dc1e80770bd28cf853
nguyenk <k.nguyen@thecodingmachine.com>
2012-12-07 15:34:33 +0100

    Initial Commit

---
+++ src/Mouf/Utils/Sesssion/SessionManager/SessionManagerInterface.php
@@ -0,0 +1,35 @@
+<?php
+namespace Mouf\Utils\Sesssion\SessionManager;
+
+/**
+ * Classes implementing this interface can manage a PHP session.
+ * The default implementation for this class is DefaultSessionManager, but you can provide your own implementation
+ * if you have a special management for the sessions.
+ *  
+ * @author David Negrier
+ */
+interface SessionManagerInterface {
+	
+	/**
+	 * Starts the session.
+	 * 
+	 * @see session_start
+	 * @return bool
+	 */
+	public function start();
+	
+	/**
+	 * Writes and closes the session
+	 * 
+	 * @see session_write_close
+	 */
+	public function write_close();
+	
+	/**
+	 * Destroys the session
+	 * 
+	 * @see session_destroy
+	 * @return bool
+	 */
+	public function destroy();
+}
---
+++ src/install.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__."/../../../autoload.php";
+
+use Mouf\Actions\InstallUtils;
+use Mouf\MoufManager;
+
+// Let's init Mouf
+InstallUtils::init(InstallUtils::$INIT_APP);
+// Let's create the instance
+$moufManager = MoufManager::getMoufManager();
+if (!$moufManager->instanceExists("sessionManager")) {
+	$moufManager->declareComponent("sessionManager", "Mouf\\Utils\\Sesssion\\SessionManager\\DefaultSessionManager");
+}
+
+// Let's rewrite the MoufComponents.php file to save the component
+$moufManager->rewriteMouf();
+
+// Finally, let's continue the install
+InstallUtils::continueInstall();
+?>
